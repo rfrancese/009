932c20ff4902258f41fd68b70d192b4ac7e89ada
Lorenzo <lorenzotufano92@gmail.com>
2014-05-08 09:52:21 +0200

    Classi per la connessione

---
+++ EcoPowerMaps/src/com/neo/ecopowermapsv1/JSONRequest.java
@@ -0,0 +1,38 @@
+package com.neo.ecopowermapsv1;
+
+import java.io.IOException;
+import java.io.UnsupportedEncodingException;
+
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpPost;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+
+public class JSONRequest {
+	
+	 //Classe che mi serve per la connessione
+	 public String getTextFromUrl(String url) {
+	        String json = null;
+	 
+	        try {
+	            DefaultHttpClient httpClient = new DefaultHttpClient();
+	            HttpPost httpPost = new HttpPost(url);
+	 
+	            HttpResponse httpResponse = httpClient.execute(httpPost);
+	            HttpEntity httpEntity = httpResponse.getEntity();
+	            json = EntityUtils.toString(httpEntity);
+	 
+	        } catch (UnsupportedEncodingException e) {
+	            e.printStackTrace();
+	        } catch (ClientProtocolException e) {
+	            e.printStackTrace();
+	        } catch (IOException e) {
+	            e.printStackTrace();
+	        }
+	        return json;
+	    
+		}
+
+}
