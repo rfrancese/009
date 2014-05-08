553840d7bf46b545c1523cc1e76489daebe4d274
Alfonso Developer <Alfonso@Neo-Unisa>
2014-05-08 10:57:39 +0200

    Richieste AsyncTask per le tre categorie e creazione e inserimento dei marker.

--- EcoPowerMaps/src/com/neo/ecopowermapsv1/MainActivity.java
+++ EcoPowerMaps/src/com/neo/ecopowermapsv1/MainActivity.java
@@ -12,10 +12,6 @@ import com.google.android.gms.maps.GoogleMap.OnInfoWindowClickListener;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.lorenzo.mappe.Indirizzo;
-import com.lorenzo.mappe.JSONRequest;
-import com.lorenzo.mappe.MainActivity;
-import com.lorenzo.mappe.R;
 
 import android.app.Dialog;
 import android.app.ProgressDialog;
@@ -40,15 +36,20 @@ public class MainActivity extends ActionBarActivity {
 	
 	private GoogleMap map;
 	private ActionBar actionBar;
-	private ArrayList<Location>listMethaneAddresses;
-	private ArrayList<Location>listGPLAddresses;
-	private ArrayList<Location>listElectricStationsAddresses;
+	private ArrayList<Location>listMethane;
+	private ArrayList<Location>listGPL;
+	private ArrayList<Location>listElectricStations;
 	private final String methaneURLRequest = "http://fanteam.altervista.org/request_methane_data.php";
 	private final String gplURLRequest = "http://fanteam.altervista.org/request_gpl_data.php";
 	private final String electricURLRequest = "http://fanteam.altervista.org/request_electric_stations_data.php";
 	private Dialog currentDialog;
 	private JSONRequest jsonRequest;
-	
+	private MethaneAsyncTask methaneRequest;
+	private GPLAsyncTask gplRequest;
+	private ElectricAsyncTask electricRequest;
+	private String address, price;
+	private String formattedAddress, provider, jacks, description;
+	private double navigateToLat, navigateToLong;
 	
 	@Override protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -125,86 +126,178 @@ public class MainActivity extends ActionBarActivity {
 		}
 		return super.onOptionsItemSelected(item);
 	}
-	
-	
-	
-	
-	 //AsyncTask per i dati
-	 public class SendData extends AsyncTask<Void,Void,ArrayList<Indirizzo>>{
 
-	private ProgressDialog dialog;
+	
+	//AsyncTask per i dati
+	public class MethaneAsyncTask extends AsyncTask<Void, Void, ArrayList<Location>> {
 
-		 protected void onPostExecute(ArrayList<Indirizzo> lista) {
+		 private ProgressDialog dialog;
 
-            marker(lista);
+		 protected void onPostExecute(ArrayList<Location> list) {
+			 marker(list);
 			 dialog.dismiss();
-
-    }
+		 }
 		 
 		 protected void onPreExecute() {
+			 dialog = new ProgressDialog(MainActivity.this);
+	         dialog.setCancelable(true);
+	         dialog.setTitle("Caricamento");
+	         dialog.setMessage("Sto contattando il server...");
+	         dialog.show();
+	     }
 
-
-	            dialog = new ProgressDialog(MainActivity.this);
-	            dialog.setCancelable(true);
-	            dialog.setTitle("Caricamento");
-	            dialog.setMessage("Sto contattando il server...Wait a moment!");
-	            dialog.show();
-
-	                }
-
-		protected ArrayList<Indirizzo> doInBackground(Void... params) {
+		 protected ArrayList<Location> doInBackground(Void... params) {
+			jsonRequest = new JSONRequest();	
 			
-			request=new JSONRequest();	
-			
-			String json = request.getTextFromUrl(URL);
+			String json = jsonRequest.getTextFromUrl(methaneURLRequest);
+
+			try {
+				JSONArray jsonArray = new JSONArray(json);
+				          
+				// looping through all item nodes <item>    
+				for (int i = 0; i < jsonArray.length(); i++) {
+				    String indirizzo = jsonArray.getJSONObject(i).getString("indirizzo");
+				    String latitudine = jsonArray.getJSONObject(i).getString("latitudine");
+				    String longitudine= jsonArray.getJSONObject(i).getString("longitudine");
+				    String prezzo=jsonArray.getJSONObject(i).getString("prezzo");
+				                
+				    //System.out.println(indirizzo+"/"+latitudine);
+				                 
+				    listMethane.add(new Location(indirizzo, prezzo, latitudine, longitudine)); // la variabile list è una lista in Java
+				 }
+			 } catch (Exception e) {
+				 e.printStackTrace();
+			 }
+				
+		return listMethane;
+		 }
+	}
+	
+	
+	
+	
+	//AsyncTask per i dati
+		public class GPLAsyncTask extends AsyncTask<Void, Void, ArrayList<Location>> {
+
+			 private ProgressDialog dialog;
+
+			 protected void onPostExecute(ArrayList<Location> list) {
+				 marker(list);
+				 dialog.dismiss();
+			 }
+			 
+			 protected void onPreExecute() {
+				 dialog = new ProgressDialog(MainActivity.this);
+		         dialog.setCancelable(true);
+		         dialog.setTitle("Caricamento");
+		         dialog.setMessage("Sto contattando il server...");
+		         dialog.show();
+		     }
+
+			 protected ArrayList<Location> doInBackground(Void... params) {
+				jsonRequest = new JSONRequest();	
+				
+				String json = jsonRequest.getTextFromUrl(gplURLRequest);
 
 				try {
-
-				   JSONArray jsonArray = new JSONArray(json);
-				          
-				   // looping through all item nodes <item>    
-				   for (int i = 0; i < jsonArray.length(); i++) {
-				                 String indirizzo = jsonArray.getJSONObject(i).getString("indirizzo");
-				                 String latitudine = jsonArray.getJSONObject(i).getString("latitudine");
-				                 String longitudine= jsonArray.getJSONObject(i).getString("longitudine");
-				                 String prezzo=jsonArray.getJSONObject(i).getString("prezzo");
-				                
-				                 System.out.println(indirizzo+"/"+latitudine);
-				                 
-				                 list.add(new Indirizzo(indirizzo,prezzo,latitudine,longitudine)); // la variabile list è una lista in Java
-				                 
-				    }
-				   
-				  
-				   
-				} catch (Exception e) {
-				    
-				     e.printStackTrace();
-				}
-				
-				return list;
-				
-			}
-			
+					JSONArray jsonArray = new JSONArray(json);
+					          
+					// looping through all item nodes <item>    
+					for (int i = 0; i < jsonArray.length(); i++) {
+					    String indirizzo = jsonArray.getJSONObject(i).getString("indirizzo");
+					    String latitudine = jsonArray.getJSONObject(i).getString("latitudine");
+					    String longitudine= jsonArray.getJSONObject(i).getString("longitudine");
+					    String prezzo=jsonArray.getJSONObject(i).getString("prezzo");
+					                
+					    //System.out.println(indirizzo+"/"+latitudine);
+					                 
+					    listGPL.add(new Location(indirizzo, prezzo, latitudine, longitudine)); // la variabile list è una lista in Java
+					 }
+				 } catch (Exception e) {
+					 e.printStackTrace();
+				 }
+					
+			return listGPL;
+			 }
+		}
 		
+		
+		
+		
+		
+		
+		//AsyncTask Colonnine Elettriche
+		public class ElectricAsyncTask extends AsyncTask<Void, Void, ArrayList<Location>> {
+
+			 private ProgressDialog dialog;
+
+			 protected void onPostExecute(ArrayList<Location> list) {
+				 markerElectricStation(list);
+				 dialog.dismiss();
+			 }
+			 
+			 protected void onPreExecute() {
+				 dialog = new ProgressDialog(MainActivity.this);
+		         dialog.setCancelable(true);
+		         dialog.setTitle("Caricamento");
+		         dialog.setMessage("Sto contattando il server...");
+		         dialog.show();
+		     }
+
+			 protected ArrayList<Location> doInBackground(Void... params) {
+				jsonRequest = new JSONRequest();	
+				
+				String json = jsonRequest.getTextFromUrl(electricURLRequest);
+
+				try {
+					JSONArray jsonArray = new JSONArray(json);
+					          
+					// looping through all item nodes <item>    
+					for (int i = 0; i < jsonArray.length(); i++) {
+					    String formattedAddress = jsonArray.getJSONObject(i).getString("IndirizzoFormattato");
+					    String latitude = jsonArray.getJSONObject(i).getString("Latitudine");
+					    String longitude = jsonArray.getJSONObject(i).getString("Longitudine");
+					    String provider =jsonArray.getJSONObject(i).getString("Provider");
+					    String jacks = jsonArray.getJSONObject(i).getString("Prese");
+					    int numStations = Integer.parseInt(jsonArray.getJSONObject(i).getString("NumPostazioni"));
+					    String description = jsonArray.getJSONObject(i).getString("Descrizione");
+					    
+					                
+					    //System.out.println(indirizzo+"/"+latitudine);
+					                 
+					    listElectricStations.add(new Location(formattedAddress, provider, jacks, latitude, longitude, numStations, description)); // la variabile list è una lista in Java
+					 }
+				 } catch (Exception e) {
+					 e.printStackTrace();
+				 }
+					
+			return listElectricStations;
+			 }
 		}
 	
-		
-		public void marker(ArrayList<Indirizzo> lista){
+	
+	
+	
+	
+	
+	
+	
+	
+	public void marker(ArrayList<Location> lista){
 			
 			for(int i=0;i<lista.size();i++){
 				
-				double latitudine= Double.parseDouble(lista.get(i).getLat());
-				double longitudine=Double.parseDouble(lista.get(i).getLong());
+				double latitudine= Double.parseDouble(lista.get(i).getLatitude());
+				double longitudine=Double.parseDouble(lista.get(i).getLongitude());
 				
-				INDI=lista.get(i).getInd();
-				PRE=lista.get(i).getPrice();
+				this.address=lista.get(i).getAddress();
+				this.price=lista.get(i).getPrice();
 				
-				System.out.println(INDI+"/"+PRE);
+				//System.out.println(INDI+"/"+PRE);
 				
 				LatLng coor= new LatLng(latitudine, longitudine);
 				
-				map.addMarker(new MarkerOptions().position(coor).title(INDI).snippet("Prezzo: "+PRE+"€"));
+				map.addMarker(new MarkerOptions().position(coor).title(this.address).snippet("Prezzo: "+this.price+"€"));
 				
 				//map.setOnMarkerClickListener(this);
 				
@@ -273,7 +366,93 @@ public class MainActivity extends ActionBarActivity {
 			
 			
 		}
-		
+	
+	
+		public void markerElectricStation(ArrayList<Location> electricList) {
+			
+			for(int i=0;i<electricList.size();i++){
+				
+				double latitudine= Double.parseDouble(electricList.get(i).getLatitude());
+				double longitudine=Double.parseDouble(electricList.get(i).getLongitude());
+				
+				this.formattedAddress=electricList.get(i).getFormattedAddress();
+				this.provider=electricList.get(i).getProvider();
+				this.jacks=electricList.get(i).getJacks();
+				this.description=electricList.get(i).getDescription();
+				
+				//System.out.println(INDI+"/"+PRE);
+				
+				LatLng coor= new LatLng(latitudine, longitudine);
+				
+				map.addMarker(new MarkerOptions().position(coor).title(this.formattedAddress).snippet("Provider: "+ this.provider
+																									+ "Prese: " + this.jacks
+																									+ "Descrizione: " + this.description));
+				
+				//map.setOnMarkerClickListener(this);
+				
+				//creo la finestra delle info
+				
+				map.setInfoWindowAdapter(new InfoWindowAdapter(){
+
+					@Override
+					public View getInfoContents(Marker marker) {
+						
+		                View v = getLayoutInflater().inflate(R.layout.finestra, null);
+		                
+		                LatLng clickMarkerLatLng = marker.getPosition();
+		                
+		                navigateToLat=clickMarkerLatLng.latitude;
+		                navigateToLong=clickMarkerLatLng.longitude;
+		                
+		                TextView title = (TextView) v.findViewById(R.id.textView1);
+		                title.setText(marker.getTitle());
+		                
+		                TextView prezzo=(TextView) v.findViewById(R.id.textView2);
+		                prezzo.setText(marker.getSnippet());
+
+		                map.setOnInfoWindowClickListener(new OnInfoWindowClickListener() {
+
+							//se clicco il marker apro un menu
+		                	
+		                	@Override
+							public void onInfoWindowClick(Marker marker) {
+								// TODO Auto-generated method stub
+		                		
+		                		//avvio il navigatore
+		                		
+		                		currentDialog=new Dialog(MainActivity.this);
+		                		currentDialog.setContentView(R.layout.scelta);
+		                		
+		                		currentDialog.setTitle("Scegli cosa fare");
+		                		
+		                		Button vai=(Button) currentDialog.findViewById(R.id.vaiqui);
+		                		Button pref=(Button) currentDialog.findViewById(R.id.pref);
+		                		
+		                		vai.setOnClickListener(l);
+		                		
+		                		currentDialog.show();
+								
+							}
+		                	
+		                	
+		                });
+						
+						return v;
+					}
+
+					@Override
+					public View getInfoWindow(Marker marker) {
+						// TODO Auto-generated method stub
+						return null;
+					}
+					
+					
+					
+					});
+				
+				
+			}
+		}
 		
 		public OnClickListener l= new OnClickListener(){
 
